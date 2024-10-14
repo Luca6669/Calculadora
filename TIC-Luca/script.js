aa9bdbcd2eb8d9931ac3b6e7a2ec840ca4c7d40f
Luca6669 <lramundo19792@iesjoanramis.org>
2024-10-14 10:40:46 +0200

    Add files via upload

---
+++ TIC-Luca/script.js
@@ -0,0 +1,25 @@
+
+function suma(){
+    let n1 = parseFloat(prompt("Dime un nombre"));
+    let n2 = parseFloat(prompt("Dime un altre"));
+    document.write(n1+n2)  
+}
+function resta(){
+    let n1 = parseFloat(prompt("Dime un nombre"));
+    let n2 = parseFloat(prompt("Dime un altre"));
+    document.write(n1-n2)  
+}
+function mult(){
+    let n1 = parseFloat(prompt("Dime un nombre"));
+    let n2 = parseFloat(prompt("Dime un altre"));
+    document.write(n1*n2)  
+}
+function div(){
+    let n1 = parseFloat(prompt("Dime un nombre"));
+    let n2 = parseFloat(prompt("Dime un altre"));
+    document.write(n1/n2)  
+}
+function arrel(){
+    let n1 = parseFloat(prompt("Dime un nombre"));
+    document.write(Math.sqrt(n1))  
+}
